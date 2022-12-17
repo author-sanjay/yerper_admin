069cad0899accfd9f1c4883fa44648611c33e76f
sanjay <sanjaykumar73189@gmail.com>
2022-12-17 16:14:54 +0530

    update

--- lib/User.dart
+++ lib/User.dart
@@ -1,15 +1,7 @@
-import 'dart:ffi';
-
 class User {
   final String email;
   final String password;
   final int id;
 
   User({required this.email, required this.password, required this.id});
-
-  User.fromJson(Map<String, dynamic> json) {
-    email = json['email'];
-    password = json['password'];
-    id = json['id'];
-  }
 }
